4a9dd447e53e8ee2449d12a76e3ed1b0449dccf3
Julian Weise <privat@julianweise.de>
2019-05-06 16:26:11 +0200

    adapted clustering in order to organize output for evaluation of choosing a good value of k for clustering

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
 
     print("[Cluster-embedded-entities] Starting clustering")
 
-    kmeans = cluster.KMeans(n_clusters=resources.constant.NUM_CLUSTERS,
+    kmeans = cluster.KMeans(n_clusters=int(args.k),
                             algorithm="elkan",
                             init="k-means++",
                             n_jobs=resources.constant.NUMBER_OF_PARALLEL_EXECUTIONS)
@@ -27,7 +27,7 @@ def main():
     print("[Cluster-embedded-entities] Clustering finished")
     print("[Cluster-embedded-entities] Start sorting entities based on assigned cluster")
 
-    cluster_map = {k: [] for k in range(resources.constant.NUM_CLUSTERS)}
+    cluster_map = {k: [] for k in range(int(args.k))}
 
     for i, word in enumerate(model.docvecs.doctags):
         cluster_map[kmeans.labels_[i]].append(word)
@@ -35,19 +35,28 @@ def main():
     print("[Cluster-embedded-entities] Sorting finished")
     print("[Cluster-embedded-entities] Start writing to file")
 
-    with open(Path(args.output, resources.constant.OUTPUT_FILE), "w+") as output_file:
-        for i in range(resources.constant.NUM_CLUSTERS):
+    # Write resulting clusters to file
+
+    output_file_location = Path(f"{args.output}/{int(args.k)}/")
+    output_file_location.mkdir(parents=True, exist_ok=True)
+    with open(Path(output_file_location, resources.constant.OUTPUT_FILE), "w+") as output_file:
+        for i in range(int(args.k)):
             closest_word = model.wv.similar_by_vector(kmeans.cluster_centers_[i], 1)[0][0]
             closest_entity = model.docvecs.most_similar([kmeans.cluster_centers_[i], 1])[0][0]
             print(f"[[CLUSTER {i}]] - Closest word: {closest_word}; Closest entity: {closest_entity}", end="\n",
                   file=output_file)
             print(*(cluster_map[i]), sep="\n", file=output_file, end="\n")
 
+    # Write performance of clustering k clusters to file
+    with open(Path(args.output, resources.constant.OUTPUT_FILE_PERFORMANCE_STATS), "a+") as performance_file:
+        print(f"{int(args.k)},{kmeans.score(x)}", file=performance_file)
+
 
 def _initialize_parser():
     general_parser = argparse.ArgumentParser(description='Clustering trained entity embeddings')
     general_parser.add_argument("--input", help='gensim model containing embedded entities',
                                 action=AccessibleTextFile, required=True)
+    general_parser.add_argument("--k", help='number of clusters to build', required=True)
     general_parser.add_argument("--output", help='Desired location for storing cluster information', required=True,
                                 action=AccessibleDirectory)
 
--- resources/constant.py
+++ resources/constant.py
@@ -1,3 +1,3 @@
-NUM_CLUSTERS = 1000
 OUTPUT_FILE = "clustered_entities.txt"
+OUTPUT_FILE_PERFORMANCE_STATS = "k_means_performance.txt"
 NUMBER_OF_PARALLEL_EXECUTIONS = 32
