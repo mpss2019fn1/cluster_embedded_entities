abdb300635fbb54408c46531ffa7890b92cd46bb
Julian Weise <privat@julianweise.de>
2019-05-06 16:46:19 +0200

    add info for current process

--- elbow_evaluation.py
+++ elbow_evaluation.py
@@ -1,5 +1,6 @@
 import os
 
 for x in range(100, 100000, 100):
+    print(f"Start clustering for k={x}")
     os.system(f"python3 main.py --input=/san2/data/teaching/chiki/wiki_living_people_model/doc2vec.binary.model --output=/san2/data/teaching/chiki/wiki_living_people_model/clustering_results --k={x}")
 
