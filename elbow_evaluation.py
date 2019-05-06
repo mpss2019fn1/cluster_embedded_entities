c00549fdd3a71c0c4475cf23ffc5a9a2f7c0e255
Julian Weise <privat@julianweise.de>
2019-05-06 16:52:57 +0200

    fix output link

--- elbow_evaluation.py
+++ elbow_evaluation.py
@@ -2,5 +2,5 @@ import os
 
 for x in range(100, 100000, 100):
     print(f"Start clustering for k={x}")
-    os.system(f"python3 main.py --input=/san2/data/teaching/chiki/wiki_living_people_model/doc2vec.binary.model --output=/san2/data/teaching/chiki/wiki_living_people_model/clustering_results --k={x}")
+    os.system(f"python3 main.py --input=/san2/data/teaching/chiki/wiki_living_people_model/doc2vec.binary.model --output=/san2/data/teaching/chiki/clustering_results --k={x}")
 
