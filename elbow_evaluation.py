fbc7021f9d98f940d4f2708e650c9d3ecaab3530
Julian Weise <privat@julianweise.de>
2019-05-06 16:45:22 +0200

    fix range for elbow

--- elbow_evaluation.py
+++ elbow_evaluation.py
@@ -1,5 +1,5 @@
 import os
 
-for x in range(10000, 100):
+for x in range(100, 100000, 100):
     os.system(f"python3 main.py --input=/san2/data/teaching/chiki/wiki_living_people_model/doc2vec.binary.model --output=/san2/data/teaching/chiki/wiki_living_people_model/clustering_results --k={x}")
 
