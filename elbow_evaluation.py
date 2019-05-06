b3159e0bf3fd83c33f6b35a2363927cc24fffbb6
Julian Weise <privat@julianweise.de>
2019-05-06 16:41:06 +0200

    add tiny script for evaluation on server

---
+++ elbow_evaluation.py
@@ -0,0 +1,5 @@
+import os
+
+for x in range(10000, 100):
+    os.system(f"python3 main.py --input=/san2/data/teaching/chiki/wiki_living_people_model/doc2vec.binary.model --output=/san2/data/teaching/chiki/wiki_living_people_model/clustering_results --k={x}")
+
